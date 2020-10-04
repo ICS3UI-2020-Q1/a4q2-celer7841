ed1c86d67a7519c0f41960817cdc28483fb8cdd1
celer7841 <celer7841@wrdsb.ca>
2020-10-04 19:18:59 +0000

    finished

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@
  *
  * @author 
  */
+ import java.util.Scanner;
 public class Main {
 
   /**
@@ -10,6 +11,13 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
-  }
+    Scanner sc = new Scanner(System.in);
+    int number = 1;
+    while(number > 0) {
+
+      System.out.println("Please enter a positive integer. Enter a negative integer to quit.");
+      number = sc.nextInt();
+    }
+
+     }
 }
